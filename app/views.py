e52bc867a1cea3c64bcc69018842be5fd47804f4
Peter-White <pwhitedeveloper@gmail.com>
2025-07-16 16:03:38 -0500

    added home views

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to Jeidikku")
+
+def about(request):
+    return HttpResponse("About Page")
