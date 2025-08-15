72d1fd3b1899200c1a53d1bda7a92ab8cf4ac791
Peter-White <pwhitedeveloper@gmail.com>
2025-08-15 15:10:05 -0500

    db name change; starting base data; fixed gitignore

--- base_chars/models.py
+++ base_chars/models.py
@@ -3,7 +3,19 @@ from django.db import models
 class Hiragana(models.Model):
     symbol = models.CharField(max_length=3, unique=True)
     romaji = models.CharField(max_length=3)
+
+    class Meta:
+        db_table = 'hiragana'
+        
+    def __str__(self):
+        return self.symbol + " (" + self.romaji + ")"
     
 class Katakana(models.Model):
     symbol = models.CharField(max_length=3, unique=True)
     romaji = models.CharField(max_length=3)
+
+    class Meta:
+        db_table = 'katakana'
+        
+    def __str__(self):
+        return self.symbol + " (" + self.romaji + ")"
