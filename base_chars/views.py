a92348fc68dbd97854440d0c198f6720b02d7fac
Peter-White <pwhitedeveloper@gmail.com>
2026-01-06 17:06:06 -0600

    list gana / kana and get routes added

--- base_chars/views.py
+++ base_chars/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, HttpResponse
 from base_chars.models import Hiragana, Katakana
+from django.core import serializers
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from base_chars import scripts
 
 def ganaList(request):
-    return HttpResponse("List Hiragana")
+    ganaList = Hiragana.objects.all()
+    json_data = serializers.serialize("json", ganaList)
+    return HttpResponse(json_data, content_type="text/json-comment-filtered")
 
 def kanaList(request):
-    return HttpResponse("List Kanagana")
+    kanaList = Katakana.objects.all()
+    json_data = serializers.serialize("json", ganaList)
+    return HttpResponse(json_data, content_type="text/json-comment-filtered")
 
 def ganaInfo(request, id):
-    return HttpResponse("Single Gana")
+    gana = Hiragana.objects.get(id=id)
+    json_data = model_to_dict(gana)
+    return JsonResponse(json_data)
 
 def kanaInfo(request, id):
-    return HttpResponse("Single Kana")
+    kana = Katakana.objects.get(id=id)
+    json_data = model_to_dict(kana)
+    return JsonResponse(json_data)
 
