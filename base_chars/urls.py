1d97ec3f0f83187c40a482b5ea3b581f03cf828f
Peter-White <pwhitedeveloper@gmail.com>
2026-01-09 14:03:57 -0600

    another mistype

--- base_chars/urls.py
+++ base_chars/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path(gana_url, base_views.ganaList, name="List of Hiragana"),
     path(kana_url, base_views.kanaList, name="List of Katakana"),
     path(gana_url + "<int:id>", base_views.ganaInfo, name="Info on Gana character"),
-    path(gana_url + "<int:id>", base_views.kanaInfo, name="Info on Kana character")
+    path(kana_url + "<int:id>", base_views.kanaInfo, name="Info on Kana character")
 ]
