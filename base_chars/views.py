fba456033fa037113f3109d3ac5d5c69dd221cd7
Peter-White <pwhitedeveloper@gmail.com>
2026-01-09 20:26:48 -0600

    group gana filter added

--- base_chars/views.py
+++ base_chars/views.py
@@ -6,7 +6,11 @@ from django.forms.models import model_to_dict
 from base_chars import scripts
 
 def ganaList(request):
-    ganaList = Hiragana.objects.all()
+    if('group_num' in request.GET):
+        ganaList = Hiragana.objects.all().filter(group_num=request.GET['group_num'])
+    else:
+        ganaList = Hiragana.objects.all()
+
     json_data = serializers.serialize("json", ganaList)
     return HttpResponse(json_data, content_type="text/json-comment-filtered")
 
@@ -25,3 +29,7 @@ def kanaInfo(request, id):
     json_data = model_to_dict(kana)
     return JsonResponse(json_data)
 
+# def ganaByGroup(request, group_num):
+#     ganaList = Hiragana.objects.all().filter(group_num=group_num)
+#     json_data = serializers.serialize("json", ganaList)
+#     return HttpResponse(json_data, content_type="text/json-comment-filtered")
