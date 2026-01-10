ccb6a5ad018cfd859af36e27a03e59ef5ba270db
Peter-White <pwhitedeveloper@gmail.com>
2026-01-10 17:18:05 -0600

    model created, json populate

--- japanese_dict/settings.py
+++ japanese_dict/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app",
-    "base_chars"
+    "base_chars",
+    "particles"
 ]
 
 MIDDLEWARE = [
--- particles/models.py
+++ particles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Particle(models.Model):
+    gana_ref = models.CharField(max_length=20, unique=True)
+    romaji = models.CharField(max_length=10, unique=True)
+    use = models.CharField(max_length=50)
+    description = models.CharField()
+
+    class Meta:
+        db_table = 'particles'
+
+    def __str__(self):
+        return self.gana_ref + " (" + self.romaji + ")"
