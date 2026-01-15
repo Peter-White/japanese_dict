95fa35be9ac14d60f33f19b33cca26a2fcd8becb
Peter-White <pwhitedeveloper@gmail.com>
2026-01-14 19:24:41 -0600

    ref fix body

--- app/scripts/reference.py
+++ app/scripts/reference.py
@@ -6,33 +6,31 @@ def ref_fetch_reg(ref):
     return re.search("^({)([a-z])(\\w+)(})$", ref)
 
 def ref_fetch(ref):
-    try:
-        reg_groups = ref_fetch_reg(ref).groups()
-        ref_obj = {}
+    reg_groups = ref_fetch_reg(ref).groups()
+    ref_obj = {}
 
-        cat = reg_groups[1]
-        id = reg_groups[2]
+    cat = reg_groups[1]
+    id = reg_groups[2]
 
-        match(cat):
-            case "h":
-                return Hiragana.objects.get(id=id)
-            case "k":
-                return Katakana.objects.get(id=id)
-            case "p":
-                fetch_part = Particle.objects.get(id=id)
-                fetch_gana = ref_fetch(fetch_part.gana_ref)
+    match(cat):
+        case "h":
+            return Hiragana.objects.get(id=id)
+        case "k":
+            return Katakana.objects.get(id=id)
+        case "p":
+            fetch_part = Particle.objects.get(id=id)
+            fetch_gana = ref_fetch(fetch_part.body)
 
-                fetch_part.gana_ref = fetch_gana.symbol
+            fetch_part.boby = fetch_gana.body
 
-                return fetch_part
+            return fetch_part
 #             case "j":
 #                 return ref_fetch_kanji(id)
-            # case "w":
-            #     return ref_fetch_word(id)
-            case _:
-                return "Error: Invalid category"
-    except AttributeError:
-        return "Error: Incorrect RegEx pattern"
+        # case "w":
+        #     return ref_fetch_word(id)
+        case _:
+            return "Error: Invalid category"
+
 #     except TypeError:
 #         return "Error: Record not found"
 
@@ -41,7 +39,12 @@ def jref(strg):
     jref_obj = {}
 
     for ind in range(len(regSpl)):
+        test = regSpl[ind]
         if ref_fetch_reg(regSpl[ind]) != None:
-            jref_obj[ind] = ref_fetch(regSpl[ind])
+            try:
+                jref_obj[ind] = ref_fetch(regSpl[ind])
+            except AttributeError:
+                continue
+
 
     return jref_obj
