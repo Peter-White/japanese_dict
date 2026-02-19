28f4f36151d6d461c3a2b50084b332573ca6e56c
Peter-White <pwhitedeveloper@gmail.com>
2026-02-19 14:21:16 -0600

    created views folder from kanji

--- kanji/urls.py
+++ kanji/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from kanji import views as kanji_views
+from kanji.views import body_views
 
 urlpatterns = [
-    path("", kanji_views.kanji_post, name="Kanji Post / Update"),
-    path("/", kanji_views.kanji_list, name="Kanji List"),
-    path("/<int:id>", kanji_views.kanji_handler_id, name="Kanji Info")
+    path("", body_views.kanji_post, name="Kanji Post / Update"),
+    path("/", body_views.kanji_list, name="Kanji List"),
+    path("/<int:id>", body_views.kanji_handler_id, name="Kanji Info")
 ]
