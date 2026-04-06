38c7b37bc62ffb99a80a90759ae5fecf58e2fcc9
Peter-White <pwhitedeveloper@gmail.com>
2026-04-06 16:57:32 -0500

    started kanji tests (do in another branch)

--- kanji/tests.py
+++ kanji/tests.py
@@ -7,6 +7,17 @@ class KanjiBodyTest(TestCase):
     def setUp(self):
         KanjiBody.objects.create(body = "日", strokes = 4)
         KanjiBody.objects.create(body = "本", strokes = 5)
+        KanjiBody.objects.create(body = "一", strokes = 1)
+        KanjiBody.objects.create(body = "木", strokes = 4)
+
+        KanjiComprised.objects.create(kanji=KanjiBody.objects.get(body="本"), order=1, body=KanjiBody.objects.get(body="一"))
+        KanjiComprised.objects.create(kanji=KanjiBody.objects.get(body="本"), order=2, body=KanjiBody.objects.get(body="木"))
+
+        KanjiDefinition.objects.create(kanji=KanjiBody.objects.get(body="一"), order=1, type="N", lang="EN", body="One")
+        KanjiDefinition.objects.create(kanji=KanjiBody.objects.get(body="本"), order=1, type="N", lang="EN", body="Book")
+        KanjiDefinition.objects.create(kanji=KanjiBody.objects.get(body="木"), order=1, type="N", lang="EN", body="Tree")
+        KanjiDefinition.objects.create(kanji=KanjiBody.objects.get(body="日"), order=1, type="N", lang="EN", body="Tree")
+        
 
     def test_kanji_body(self):
         ni = KanjiBody.objects.get(body = "日")
