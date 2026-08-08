949a20e0ade193f3de096104b57ea48988267e5a
Peter-White <pwhitedeveloper@gmail.com>
2026-08-08 16:40:47 -0500

    large model props

--- large/models.py
+++ large/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+DEFINITION_LANGS = [
+    ("EN", "English")
+]
+
+class LargeBody(models.Model):
+    body = models.CharField(unique=True)
+
+    class Meta:
+        db_table = 'large_bodies'
+
+    def get_id(self):
+        return self.pk
+
+    def get_body(self):
+        return self.body
+    
+    def set_body(self, body):
+        self.body = body
+
+    @property
+    def to_dict(self):
+        obj = {}
+        obj["id"] = self.pk
+        obj["cat"] = "large"
+        obj["body"] = self.body
+        return obj
+
+class LargeTranslation(models.Model):
+    large = models.ForeignKey(LargeBody, verbose_name=("Translation"), on_delete=models.CASCADE, null=True)
+    order = models.IntegerField()
+    lang = models.CharField(max_length=2, choices=DEFINITION_LANGS, default="EN")
+    body = models.TextField()
+
+    class Meta:
+        db_table = 'large_translations'
+
+    def get_id(self):
+        return self.pk
+
+    def get_body(self):
+        return self.body
+    
+    def set_body(self, body):
+        self.body = body
+
+    def get_order(self):
+        return self.order
+    
+    def set_order(self, order):
+        self.order = order
+
+    def get_large(self):
+        return self.large
+    
+    def set_large(self, large):
+        self.large = large
+
+    def get_lang(self):
+        return self.lang
+    
+    def set_lang(self, lang):
+        self.lang = lang
+
+    @property
+    def to_dict(self):
+        obj = {}
+        obj["id"] = self.pk
+        obj["order"] = self.order
+        obj["lang"] = self.lang
+        obj["body"] = self.body
+        return obj
